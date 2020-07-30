0a1081c1ffbf81f131b6b76577204460b459b74b
Dany Diaz <dany.diaz91@hotmail.com>
2020-07-30 01:41:35 -0500

    feat: add repository trait

--- bicycles-core/src/models.rs
+++ bicycles-core/src/models.rs
@@ -1,5 +1,8 @@
 use chrono::prelude::{DateTime, Utc};
 use uuid::Uuid;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
 pub struct Bicycle {
     id: Uuid,
     owner_id: Uuid,
